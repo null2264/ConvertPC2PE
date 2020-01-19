19f0ff7dc008d4d513362b665874d81cf171c2a7
ziro <palembani@gmail.com>
2020-01-19 12:32:54 +0700

    + Added some more texture name

--- ConvertPC2PE.py
+++ ConvertPC2PE.py
@@ -25,6 +25,8 @@ pngdirPE="/home/ziro/my Files/PE Port Project/" + inPEResource + "/textures/bloc
 
 input0=1
 error=0
+blockOK=0
+blockNO=0
 
 print("")
 print("LOGS:")
@@ -45,12 +47,18 @@ while input0 < len(pcfile):
         else:
             print(bcolors.OKGREEN + "OK!", bcolors.ENDC + pcfile[input0], "Successfully converted,", "renamed to", pefile[input0])
         input0 = input0 + 1
+        blockOK = blockOK + 1
     except: # Keeps going even if some files is not found, also print out which file it is"
         pass
         print(bcolors.FAIL + "ERROR!", bcolors.ENDC + "The following file:", pcfile[input0], "is not found, skipped")
         input0 = input0 + 1
         error = error + 1
+        blockNO = blockNO + 1
 print("---------------------------------------------")
+print(bcolors.BOLD + "Summary" + bcolors.ENDC)
+print("---------------------------------------------")
+print(blockOK, "blocks successfully converted")
+print(blockNO, "blocks failed being converted")
 if error == 1: # Print out how many errors there are
     print("Convertion completed with", error, "error")
 elif error >=2:
